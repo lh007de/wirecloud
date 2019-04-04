808c1ae5dc70e0dcf8677d530cd64a5b97a4cc51
lh <1552129532@qq.com>
2019-04-04 08:47:37 +0800

    增加了分享图片部分功能 v0.1

--- src/pages/ordercommiteddetail/index.vue
+++ src/pages/ordercommiteddetail/index.vue
@@ -74,7 +74,7 @@
       </div>
     </div>
     <div class="mybox">
-      <canvas canvas-id="shareCanvas" style="width:600rpx;height:900rpx"></canvas>
+      <canvasdrawer :painting="painting" class="canvasdrawer" @getImage="eventGetImage"></canvasdrawer>
     </div>
   </div>
 </template>
@@ -111,15 +111,176 @@
         service_end_time: formatTime(new Date()),
         service_payment_cycle: '按年',
         dataURL: '',
-        siteURL: 'http://www.cqyzxpt.cn'
+        siteURL: 'http://www.cqyzxpt.cn',
+        pixelRatio: 0,
+        windowWidth: 500,
+        windowHeight: 500,
+        painting: '',
+        shareImage: '',
+        fontsize_title: 18,
+        fontsize_content: 14,
+        divide: 4
 
       }
     },
     mounted () {
     },
     methods: {
+      eventGetImage (e) {
+        console.log(e.mp.detail)
+        wx.hideLoading()
+        this.shareImage = e.mp.detail.tempFilePath
+      },
       share () {
         // 这里需要用到微信提供的canvas进行绘图，mpvue 没有dom概念，比较坑，不能用现成工具
+        wx.showLoading({
+          title: '绘制分享图片中',
+          mask: true
+        })
+        this.painting = {
+          width: 375,
+          height: 555,
+          clear: true,
+          views: [
+            {
+              type: 'image',
+              url: '../images/backgroud.png',
+              top: 0,
+              left: 0,
+              width: 375,
+              height: 555
+            },
+            {
+              type: 'image',
+              url: '../images/picsharehead.png',
+              top: 0,
+              left: 0,
+              width: 375,
+              height: 172
+            },
+
+            {
+              type: 'rect',
+              background: '#f5f5f6',
+              top: 180,
+              left: 0,
+              width: 375,
+              height: 100
+            },
+
+            {
+              type: 'text',
+              content: 'A端',
+              fontSize: this.fontsize_title,
+              color: '#212121',
+              textAlign: 'left',
+              top: 180,
+              left: 150
+            },
+            {
+              type: 'text',
+              content: '站点地址:' + this.site_address_A,
+              fontSize: this.fontsize_content,
+              color: '#383549',
+              textAlign: 'left',
+              top: 200,
+              left: 50
+            },
+            {
+              type: 'text',
+              content: '站点联系人:' + this.site_name_A,
+              fontSize: this.fontsize_content,
+              color: '#383549',
+              textAlign: 'left',
+              top: 220,
+              left: 50
+            },
+            {
+              type: 'text',
+              content: '站点联系人邮箱:' + this.site_email_A,
+              fontSize: this.fontsize_content,
+              color: '#383549',
+              textAlign: 'left',
+              top: 240,
+              left: 50
+            },
+            {
+              type: 'text',
+              content: '站点联系人电话:' + this.site_phone_A,
+              fontSize: this.fontsize_content,
+              color: '#383549',
+              textAlign: 'left',
+              top: 260,
+              left: 50
+            },
+
+            {
+              type: 'rect',
+              background: '#f5f5f6',
+              top: 288,
+              left: 0,
+              width: 375,
+              height: 100
+            },
+
+            {
+              type: 'text',
+              content: 'Z端',
+              fontSize: this.fontsize_title,
+              color: '#212121',
+              textAlign: 'left',
+              top: 288,
+              left: 150
+            },
+            {
+              type: 'text',
+              content: '站点地址:' + this.site_address_Z,
+              fontSize: this.fontsize_content,
+              color: '#383549',
+              textAlign: 'left',
+              top: 308,
+              left: 50
+            },
+            {
+              type: 'text',
+              content: '站点联系人:' + this.site_name_Z,
+              fontSize: this.fontsize_content,
+              color: '#383549',
+              textAlign: 'left',
+              top: 328,
+              left: 50
+            },
+            {
+              type: 'text',
+              content: '站点联系人邮箱:' + this.site_email_Z,
+              fontSize: this.fontsize_content,
+              color: '#383549',
+              textAlign: 'left',
+              top: 348,
+              left: 50
+            },
+            {
+              type: 'text',
+              content: '站点联系人电话:' + this.site_phone_Z,
+              fontSize: this.fontsize_content,
+              color: '#383549',
+              textAlign: 'left',
+              top: 368,
+              left: 50
+            }
+          ]
+        }
+        wx.hideLoading()
+        wx.saveImageToPhotosAlbum({
+          filePath: this.shareImage,
+          success (res) {
+            wx.showToast({
+              title: '保存图片成功',
+              icon: 'success',
+              duration: 2000
+            })
+          }
+        })
       },
       copyURL () {
         let that = this
